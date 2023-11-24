1d285c2bcc1b196861a0038d66b66bdfabb5a6b6
rallsbrown <95102991+rallsbrown@users.noreply.github.com>
2023-11-24 13:56:30 -0800

    get route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .get("/", () => "Hello Bun, dev. I'm going to build RESTFUL API")
+  .get("/post/:id", ({ params: { id } }) => {
+    return { id: id, title: "Learn Bun" };
+  })
   .listen(3000);
 
 console.log(
